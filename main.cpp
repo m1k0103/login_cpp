4b92e314ed4915b14417bdbda6e424f78cd01f70
miki <84585119+miko1034@users.noreply.github.com>
2024-12-19 09:36:42 +0000

    sqlite3 works kinda but error

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-#include "imgui.h"
-using namespace std;
+//#include "imgui.h"
+#include <sqlite3.h>
+
+
+int check_error(int exit, char* messageError){
+	if (exit != SQLITE_OK){
+		std::cerr << "Error" << '\n';
+	}
+	else{
+		std::cout << "No error performing operation." << '\n';
+	}
+}
 
 
 
+int main() {
+	sqlite3* DB;
+	int exit = 0;
+	exit = sqlite3_open("example.db", &DB);
+	char* messageError;
 
-int main(){
+	string sql_query = "CREATE TABLE test(id int primary key not null autoincrement,name text, surname text);";
+	exit = sqlite3_exec(DB,sql_query.c_str(),NULL,0,&messageError);
+	check_error(exit, messageError);
 
+	
+
+	return 0;
 
 }
